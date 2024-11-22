af6863485a84083f0fd9ff196e8f1e7c32468609
Shehab Alsharabi <shehabhamood102@gmail.com>
2024-11-22 21:32:19 +0300

    UPDATE 1

--- ass-2/index.php
+++ ass-2/index.php
@@ -14,9 +14,45 @@ $data = json_decode($response, true);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     
     <style>
-        
-       
-        
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f9;
+        }
+
+        header {
+            background-color: #333;
+            color: white;
+            padding: 1rem;
+            text-align: center;
+        }
+
+        h1 {
+            font-size: 2rem;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin: 20px 0;
+            font-size: 1em;
+            font-family: Arial, sans-serif;
+        }
+
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 12px;
+            text-align: left;
+        }
+
+
+        th {
+            background-color: gray;
+            color: white;
+            text-transform: uppercase;
+        }
     </style>
 
 </head>
@@ -35,7 +71,16 @@ $data = json_decode($response, true);
                 </tr>
             </thead>
             <tbody>
-                
+            <?php foreach($data['results'] as $result): ?> 
+                    <tr>
+                        <td><?= htmlspecialchars($result['year']) ?></td>
+                        <td><?= htmlspecialchars($result['semester']) ?></td>
+                        <td><?= htmlspecialchars($result['the_programs']) ?></td>
+                        <td><?= htmlspecialchars($result['nationality']) ?></td>
+                        <td><?= htmlspecialchars($result['colleges']) ?></td>
+                        <td><?= htmlspecialchars($result['number_of_students']) ?></td>
+                    </tr>              
+            <?php endforeach; ?>
             </tbody>
         </table>
     </main>
